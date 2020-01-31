4a483241613cc3943403929252f1a885567ecc89
Jhon Emil Tigbayan <jetigbayan@gmail.com>
2020-01-31 11:28:00 +0800

    Update login.spec.js
    refactor code for better readbility and rendering

--- cypress/integration/modules/login.spec.js
+++ cypress/integration/modules/login.spec.js
@@ -1,36 +1,36 @@
 const email = 'admin@primepay.ph';
 const password = '123123123';
 
-describe('function name', function() {
+describe('Log in page', function() {
 
-  it('function description', function() {
+  it('Will go to the landing page of AllEasy core', function() {
     cy.visit('http://staging-afcs-core.primepay.com.ph/')
 
     })
 })
 
-describe('function name', function() {
+describe('Input Email', function() {
     
-    it('function description', function() {
+    it('Should enter the username of the user', function() {
         cy.get('[name=email]').click().type(email)
-    
+        .wait(1000)
     })
 })
 
-describe('function name', function() {
+describe('Input Password', function() {
     
-    it('function description', function() {
+    it('Should enter the password of the user', function() {
         cy.get('[name=password]').click().type(password)
-    
+        .wait(1000)
     })
 })
 
 
-describe('function name', function() {
+describe('Submit credentials', function() {
     
-    it('function description', function() {
+    it('should click submit to go to the dashboard', function() {
         cy.get('[id=btn]').click()
-    
+        .wait(1000)
     })
 })
 
